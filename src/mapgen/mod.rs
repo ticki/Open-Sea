9ffa82813f626c6f29733baac7021134223ec3ed
Ticki <Ticki@users.noreply.github.com>
2015-08-01 09:41:54 +0200

    Implement BigChuncks

--- src/mapgen/mod.rs
+++ src/mapgen/mod.rs
@@ -10,9 +10,25 @@ use traits::{Tile, TileMap};
 // Probably really buggy code...
 
 /// A map
+///
+/// Note: It's chuncked into big chuncks and small chuncks.
+///       Small chuncks keeps the noise data. Big chuncks
+///       determines the overlay layer.
 pub struct MapGenerator<'a> {
   seed: &'a Seed,
 }
+
+/// Types of big chuncks
+enum BChunckType {
+  /// Reserved for history, empty per default, but gets opened as the game is played. Manually designed.
+  History,
+  // Automatic map generated chunck
+  Auto,
+  // Manually random generated chunck
+  Manually,
+}
+
+// TODO: Find out how to prevent double islands (manually generated islands)
 
 impl<'a> MapGenerator<'a> {
   /// Creates a new map
@@ -29,6 +45,19 @@ impl<'a> MapGenerator<'a> {
     let noise = Brownian2::new(open_simplex2, 4).wavelength(32.0);
     noise.apply(&self.seed, &[x as f64, y as f64])
   }
+
+  /// Get big chunck coordinates
+  fn get_bchunck(x: i64, y: i64) -> (i64, i64) {
+    // TODO: Chunck size 64?
+    (((x as f64) / 64.0).floor() as i64, ((y as f64) / 64.0).floor() as i64)
+  }
+
+  /* TODO: Finish this:
+  fn get_bchunck_type(x: i64, y: i64) -> BChunckType {
+    let noise = Brownian2::new(open_simplex2, 4).wavelength(32.0);
+    noise.apply(&self.seed, &[x as f64, y as f64])
+  }
+  */
 
   /// Get overlay value (used for customizing the noise)
   fn get_overlay_value(&self, x: i64, y: i64) -> f64 {
