4385f584206b42d16d759b75206ba590475ebce1
Adam Badawy <adambada@buffalo.edu>
2015-08-02 17:17:59 -0400

    Fix Character/Texture ownership problem

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -24,10 +24,30 @@ const FONT_CHARS_IN_COL: u32 = 16;
 const FONT_UNKNOWN_GLYPH_CHAR: char = '?';
 
 
+#[derive(Clone, Copy, Debug)]
+struct TextureData {
+  size: (u32, u32)
+}
+
+
+impl TextureData {
+  pub fn new(texture: &opengl_graphics::Texture) -> TextureData {
+    TextureData { size: texture.get_size() }
+  }
+}
+
+
+impl ImageSize for TextureData {
+  fn get_size(&self) -> (u32, u32) {
+    self.size
+  }
+}
+
+
 struct Font {
   char_size: (u32, u32),
   texture: opengl_graphics::Texture,
-  cache: HashMap<u32, Character<opengl_graphics::Texture>>,
+  cache: HashMap<u32, Character<TextureData>>,
 }
 
 
@@ -42,14 +62,14 @@ impl Font {
     }
   }
 
-  fn create_character(&self, code: u32) -> Character<opengl_graphics::Texture> {
+  fn create_character(&self, code: u32) -> Character<TextureData> {
     let (cw, ch) = self.char_size;
     let left_offs = (code % FONT_CHARS_IN_ROW) * cw;
     let top_offs = (code / FONT_CHARS_IN_ROW) * ch;
     Character {
       offset: [left_offs as f64, top_offs as f64],
       size: [cw as f64, ch as f64],
-      texture: self.texture,
+      texture: TextureData::new(&self.texture),
     }
   }
 }
@@ -57,7 +77,7 @@ impl Font {
 
 impl CharacterCache for Font {
 
-  type Texture = opengl_graphics::Texture;
+  type Texture = TextureData;
 
   fn character(&mut self,
                font_size: graphics::types::FontSize,
