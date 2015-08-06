79d50cce1dba60a10d5a99daa0008c4ebc2f24d3
Ticki <Ticki@users.noreply.github.com>
2015-08-06 16:43:09 +0200

    Add object module

---
+++ src/core/object.rs
@@ -0,0 +1,103 @@
+use opengl_graphics::*;
+use core::{Vec2};
+
+/// A trait for positioned objects
+pub trait Positioned {
+  /// Get the x coordinate
+  fn get_pos(&self) -> Vec2<i64>;
+  /// Set the Vec coordinate
+  fn set_pos(&mut self, new_pos: Vec2<i64>);
+}
+
+/// The direction of a given object
+#[derive(Clone, Copy)]
+pub enum Dir {
+  Left,
+  Right,
+  Up,
+  Down,
+}
+impl Dir {
+  fn to_vec(&self) -> Vec2<i64> {
+    Vec2(
+      match *self {
+        Dir::Left => 1,
+        Dir::Right => -1,
+        _ => 0,
+      },
+
+      match *self {
+        Dir::Up => 1,
+        Dir::Down => -1,
+      _ => 0,
+      }
+    )
+  }
+}
+
+// TODO: Implement two directions at once.
+// TODO: Make drawable trait
+
+/// A movable object
+pub trait Movable: Positioned {
+  /// Get the direction
+  fn get_dir(&self) -> Dir;
+  /// Set the direction
+  fn set_dir(&mut self, new_dir: Dir);
+  /// Is the object moving?
+  fn is_moving(&self) -> bool;
+  /// Move the object
+  fn move_obj(&mut self, mov: Vec2<i64>) {
+    let coord = self.get_pos();
+    self.set_pos(coord + mov);
+  }
+  /// Get new coordinate
+  fn get_new_pos(&self) -> Vec2<i64> {
+    let dir = self.get_dir();
+    self.get_pos() + dir.to_vec()
+  }
+  /// Move object in direction.
+  fn move_obj_dir(&mut self) {
+    let new_coord = self.get_new_pos();
+    self.set_pos(new_coord)
+  }
+}
+
+/// Trait for animated objects
+pub trait Animated: Movable {
+  /// Get transitition point, which is in the interval [0,1]
+  fn get_trans_state(&self) -> f64;
+  /// Get animation frame
+  fn get_animation_frame(&self) -> i16;
+}
+
+/// Trait for sprited objects
+pub trait Sprited: Animated {
+  /// Get current sprite
+  fn get_sprite(&self) -> &Texture;
+  /// Get width, not neccesarily the width of the sprite, but rather the space
+  /// the given object occupies. Given in fields.
+  fn get_width(&self) -> i16;
+  /// Get height, see note above
+  fn get_height(&self) -> i16;
+  /// Get the opacity of the object
+  fn get_opacity(&self) -> f64;
+
+  // TODO: Add draw method.
+}
+
+// TODO: Add event trait, for objects you can click on etc.
+
+/// Entity ID type
+pub struct Id(pub i64);
+
+/// An entity
+pub trait Entity: Sprited {
+  /// Get the ID of the given entity
+  fn id(&self) -> Id;
+  /// Is the entity solid at point (x, y) relative to the position?
+  fn is_solid(&self, x: i16, y: i16) -> bool;
+  /// Update the entity
+  fn update(&mut self, dt: f64);
+  // Probably need more methods.
+}
