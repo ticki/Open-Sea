3063de1b151775810f95d56654a96263d891f743
Ticki <Ticki@users.noreply.github.com>
2015-08-11 18:56:17 +0200

    Update script

--- docupload.sh
+++ docupload.sh
@@ -1,8 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-[ $TRAVIS_BRANCH = master ] &&
-[ $TRAVIS_PULL_REQUEST = false ] &&
-cargo doc &&
-echo "<meta http-equiv=refresh content=0;url=`echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2`/index.html>" > target/doc/index.html &&
-mv target/doc ./
-git push -f https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+if [ -z "$GH_TOKEN" ]; then
+  echo "No token available for pushing the docs."
+elif [ "$TRAVIS_REPO_SLUG" == "$RD_PUSH_REPO" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "$RD_PUSH_BRANCH" ]; then
+
+  echo "Rust doc upload..."
+
+  cargo doc --no-deps
+  echo "<meta http-equiv=refresh content=0;url=$RD_INDEX_PAGE>" > target/doc/index.html
+  pip install --user ghp-import
+  ~/.local/bin/ghp-import -n target/doc
+  git push -qf https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+
+  echo "Docs uploaded."
+else
+  echo "Error: Couldn't upload docs"
+
+fi
+
