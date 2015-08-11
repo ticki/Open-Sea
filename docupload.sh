8befb2bf8d0d84d6c60e3accba9c0edcf5e9205a
Ticki <Ticki@users.noreply.github.com>
2015-08-11 17:34:36 +0200

    Fix

--- docupload.sh
+++ docupload.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-  [ $TRAVIS_BRANCH = master ] &&
-  [ $TRAVIS_PULL_REQUEST = false ] &&
-  cargo doc &&
-  echo "<meta http-equiv=refresh content=0;url=`echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2`/index.html>" > target/doc/index.html &&
-  mv target/doc ./
-  git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+[ $TRAVIS_BRANCH = master ] &&
+[ $TRAVIS_PULL_REQUEST = false ] &&
+cargo doc &&
+echo "<meta http-equiv=refresh content=0;url=`echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2`/index.html>" > target/doc/index.html &&
+mv target/doc ./
+git push -f https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
