f20831c84eb1afda5e71b2e2cc861a81ede901a8
Ticki <Ticki@users.noreply.github.com>
2015-08-11 17:12:29 +0200

    Die, bug, die!

--- docupload.sh
+++ docupload.sh
@@ -4,6 +4,5 @@
   [ $TRAVIS_PULL_REQUEST = false ] &&
   cargo doc &&
   echo "<meta http-equiv=refresh content=0;url=`echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2`/index.html>" > target/doc/index.html &&
-  sudo pip install ghp-import &&
-  ghp-import -n target/doc &&
+  mv target/doc ./
   git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
